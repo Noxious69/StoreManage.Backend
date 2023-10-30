cf940b32144d1750088341a6a86566a57135c78b
alishs <shabaisuorosh@gmail.com>
2023-10-30 12:12:53 +0330

    initial

---
+++ Inventory.Core/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Core.Entities
+{
+    public class Order
+    {
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerPhone { get; set; }
+
+        public string OrderType { get; set; }
+        public string OrderColor { get; set; }
+        public int OrderCount { get; set; }
+
+        public string BoxType { get; set; }
+        public string BoxCount { get; set; }
+
+        public bool Edge { get; set; }
+        public bool Lable { get; set; }
+        public bool TwoColor { get; set; }
+    }
+}
